7e220b9e3560c23292df6480793a84d20f0ca8ef
moradalkhatib <muradalkhateb@icloud.com>
2021-11-27 16:43:39 +0200

    fix some issues

--- books_api/views.py
+++ books_api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import serializers
+from rest_framework import permissions, serializers
 
 from books_api_project.settings import REST_FRAMEWORK
 
@@ -7,12 +7,16 @@ from books_api_project.settings import REST_FRAMEWORK
 from rest_framework.generics import ListCreateAPIView , RetrieveUpdateDestroyAPIView
 from .models import Book
 from .serializers import BookSterilizer
+from .permissions import IsAuthorOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 class BookList(ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSterilizer
+    permissions_classes = (IsAuthenticated,)
 
 
 class BookDetail(RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSterilizer
+    permission_classes = (IsAuthorOrReadOnly,)
